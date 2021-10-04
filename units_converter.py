e4a4d8e2dc49c5d34ddebf9201a9647836d437c4
Anna Toidze <toidzeanna@gmail.com>
2021-10-04 16:42:32 +0200

    Changed the factor in atm_to_pascal, changed cup_to_cubicmeter to cup_to_ml, added pound_to_killogram

--- units_converter.py
+++ units_converter.py
@@ -6,11 +6,13 @@ def atm_to_bar(value):
     """
     return value*1.01325
 
+
 def bar_to_atm(value):
     """
     Converts bar to atmosphere
     """
     return value/1.01325
+
 
 def bar_to_pascal(value):
     """
@@ -18,13 +20,15 @@ def bar_to_pascal(value):
     """
     return value*10**5
 
+
 def atm_to_pascal(value):
     """
     Converts atmosphere to pascal
     """
-    return value*101325
+    return atm_to_bar(bar_to_pascal(value))
 
-#American measurement system to SI Units
+# American measurement system to SI Units
+
 
 def inch_to_km(value):
     """
@@ -32,11 +36,13 @@ def inch_to_km(value):
     """
     return value/39370
 
+
 def feet_to_km(value):
     """
     Converts feet to kilometer (Distance)
     """
     return value/3281
+
 
 def mile_to_km(value):
     """
@@ -44,21 +50,30 @@ def mile_to_km(value):
     """
     return value*1.60934
 
+
 def yard_to_km(value):
     """
     Converts yards to kilometer (Distance)
     """
     return value/1094
 
+
 def fahrenheit_to_kelvin(value):
     """
     Converts fahrenheit to kelvin (Temperature)
     """
-    return (value - 32)*5/9+ 273.15
+    return (value - 32)*5/9 + 273.15
 
-def cup_to_cubicmeter(value):
-    """
-    Converts cup to cubic meter (Volume)
-    """
-    return value/4427
 
+def cup_to_ml(value):
+    """
+    Converts cup to milliliter (Volume)
+    """
+    return value*236.588
+
+
+def pound_to_kilogram(value):
+    """
+    Converts pounds to kilograms
+    """
+    return value/2.205
