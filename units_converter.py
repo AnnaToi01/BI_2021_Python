c0986f476c0183b38e0147e26effe91101b714af
Anna Toidze <toidzeanna@gmail.com>
2021-10-04 17:30:34 +0200

    Creating conflict, changed function inch_to_km to inch_to_cm, fahrenheit_to_kelvin to fahrenheit_to_celsius and added gallon_to_liter

--- units_converter.py
+++ units_converter.py
@@ -6,11 +6,13 @@ def atm_to_bar(value):
     """
     return value*1.01325
 
+
 def bar_to_atm(value):
     """
     Converts bar to atmosphere
     """
     return value/1.01325
+
 
 def bar_to_pascal(value):
     """
@@ -18,19 +20,23 @@ def bar_to_pascal(value):
     """
     return value*10**5
 
+
 def atm_to_pascal(value):
     """
     Converts atmosphere to pascal
     """
     return value*101325
 
-#American measurement system to SI Units
 
-def inch_to_km(value):
+# American measurement system to SI Units
+
+
+def inch_to_cm(value):
     """
     Converts inches to kilometer (Distance)
     """
-    return value/39370
+    return value*2.54
+
 
 def feet_to_km(value):
     """
@@ -38,11 +44,13 @@ def feet_to_km(value):
     """
     return value/3281
 
+
 def mile_to_km(value):
     """
     Converts mile to kilometer (Distance)
     """
     return value*1.60934
+
 
 def yard_to_km(value):
     """
@@ -50,15 +58,23 @@ def yard_to_km(value):
     """
     return value/1094
 
-def fahrenheit_to_kelvin(value):
+
+def fahrenheit_to_celsius(value):
     """
     Converts fahrenheit to kelvin (Temperature)
     """
-    return (value - 32)*5/9+ 273.15
+    return (value - 32)*5/9
+
 
 def cup_to_cubicmeter(value):
     """
     Converts cup to cubic meter (Volume)
     """
     return value/4427
+
+def gallon_to_liter(value):
+    """
+    Converts cup to cubic meter (Volume)
+    """
+    return value*3.78541
 
