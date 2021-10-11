eee555e3a3389716b2daf36a52ce0c71db4ebd0f
Anna Toidze <toidzeanna@gmail.com>
2021-10-11 10:39:47 +0200

    Added some features of interface

--- units_converter.py
+++ units_converter.py
@@ -1,33 +1,3 @@
-# Pressure converter
-
-def atm_to_bar(value):
-    """
-    Converts atmosphere to bar
-    """
-    return value*1.01325
-
-
-def bar_to_atm(value):
-    """
-    Converts bar to atmosphere
-    """
-    return value/1.01325
-
-
-def bar_to_pascal(value):
-    """
-    Converts bar to pascal
-    """
-    return value*10**5
-
-
-def atm_to_pascal(value):
-    """
-    Converts atmosphere to pascal
-    """
-    return value*101325
-
-
 # American measurement system to SI Units
 
 
@@ -35,34 +5,35 @@ def inch_to_cm(value):
     """
     Converts inches to kilometer (Distance)
     """
+    if value < 0:
+        return "Measured distance cannot be less than 0"
     return value*2.54
 
 
-def feet_to_km(value):
+def feet_to_m(value):
     """
-    Converts feet to kilometer (Distance)
+    Converts feet to meter (Distance)
     """
-    return value/3281
+    if value < 0:
+        return "Measured distance cannot be less than 0"
+    return value/3.281
 
 
 def mile_to_km(value):
     """
     Converts mile to kilometer (Distance)
     """
+    if value < 0:
+        return "Measured distance cannot be less than 0"
     return value*1.60934
-
-
-def yard_to_km(value):
-    """
-    Converts yards to kilometer (Distance)
-    """
-    return value/1094
 
 
 def fahrenheit_to_celsius(value):
     """
     Converts fahrenheit to kelvin (Temperature)
     """
+    if value < -459.67:
+        return "You've reached the absolute minimum of temperature"
     return (value - 32)*5/9
 
 
@@ -70,6 +41,8 @@ def cup_to_cubicmeter(value):
     """
     Converts cup to cubic meter (Volume)
     """
+    if value < 0:
+        return "Volume cannot be less than 0"
     return value/4427
 
 
@@ -77,4 +50,34 @@ def gallon_to_liter(value):
     """
     Converts cup to cubic meter (Volume)
     """
+    if value < 0:
+        return "Volume cannot be less than 0"
     return value*3.78541
+
+
+dic_func= {
+    "fahrenheit": fahrenheit_to_celsius,
+    "inch": inch_to_cm,
+    "feet": feet_to_m,
+    "mile": mile_to_km,
+    "cup": cup_to_cubicmeter,
+    "gallon": gallon_to_liter
+}
+
+
+in_units = ["inch", "feet", "mile", "fahrenheit", "cup", "gallon"]
+out_units = ["centimeter", "meter", "kilometer", "celsius", "cubicmeter", "liter"]
+
+try:
+    in_unit = input("Enter an input unit: ").lower()
+    out_unit = input("Enter an output unit: ").lower()
+    in_value = float(input("Enter the value: "))
+    if in_units.index(in_unit) == out_units.index(out_unit):
+        print(dic_func[in_unit](in_value))
+    else:
+        Exception
+except:
+    print("False units. Possible unit conversions are:\ninch to centimeter\nfeet to meter\nmile to kilometer\nyard to kilometer\nfahrenheit to celsius\ncup to cubicmeter\ngallon to liter")
+
+
+
