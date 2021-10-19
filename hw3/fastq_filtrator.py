d1105c552c77ec517e87917b516675953625a2b3
Anna Toidze <toidzeanna@gmail.com>
2021-10-19 15:52:41 +0200

    Changing variable names as asked for in the exercise

--- hw3/fastq_filtrator.py
+++ hw3/fastq_filtrator.py
@@ -1,7 +1,7 @@
 import os
 
 
-def united(reads, dic_ind, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), qs_threshold=0):
+def united(reads, dic_ind, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0):
     """
     Checking mean GC content by specifying the bounds of it in gc_bounds, if just one threshold - taken as max_value
     Checking mean length by specifying the bounds of it in length_bounds, if just one threshold - taken as max_value
@@ -27,16 +27,16 @@ def united(reads, dic_ind, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), qs_th
             if len(read[1]) > ln_up or len(read[1]) < ln_low:
                 dic_ind[i] += 1
                 print("Hi")
-        if qs_threshold != 0:
-            qs_sum = 0
+        if quality_threshold != 0:
+            quality_sum = 0
             for character in read[3]:
-                qs_sum += ord(character) - 33
-            qs_mean = qs_sum / len(read[3])
-            if qs_mean < qs_threshold:
+                quality_sum += ord(character) - 33
+            quality_mean = quality_sum / len(read[3])
+            if quality_mean < quality_threshold:
                 dic_ind[i] += 1
 
 
-def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), qs_threshold=0,
+def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0,
          save_filtered=False):
     passed_reads = output_file_prefix + "_passed.fastq"
     failed_reads = output_file_prefix + "_failed.fastq"
@@ -47,7 +47,7 @@ def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 
             reads.append(fastq[index:index + 4])
         dic_ind = {i: 0 for i in range(len(reads))}
         # Uniting all the functions - in this way, looping once through all the reads should be possible:
-        united(reads, dic_ind, gc_bounds, length_bounds, qs_threshold)
+        united(reads, dic_ind, gc_bounds, length_bounds, quality_threshold)
         for key, value in dic_ind.items():
             if value > 0:
                 if save_filtered:
@@ -73,9 +73,9 @@ if __name__ == "__main__":
     output_path = input("Enter the prefix of the output file: ")
     gc_bounds_in = tuple(float(i) for i in input("Enter the bounds for GC: ").split())
     length_bounds_in = tuple(int(i) for i in input("Enter the bounds for length of the read: ").split())
-    qs_threshold_in = input("Enter the minimal mean quality of the read: ")
+    quality_threshold_in = input("Enter the minimal mean quality of the read: ")
     try:
-        qs_threshold_in = float(qs_threshold_in)
+        quality_threshold_in = float(quality_threshold_in)
     except ValueError:
         pass
     save_filtered_in = bool(int(input("Should reads that failed be saved? Yes - 1, No - 0 \n")))
@@ -84,10 +84,10 @@ if __name__ == "__main__":
     # output_path = "test"
     # gc_bounds_in = tuple(float(i) for i in "40 60".split())
     # length_bounds_in = tuple(float(i) for i in "20 110".split())
-    # qs_threshold_in = float("30")
+    # quality_threshold_in = float("30")
     # save_filtered_in = bool(int("0"))
     kwargs = dict(gc_bounds=gc_bounds_in, length_bounds=length_bounds_in,
-                  qs_threshold=qs_threshold_in, save_filtered=save_filtered_in)
+                  quality_threshold=quality_threshold_in, save_filtered=save_filtered_in)
 
     main(input_path, output_path,
          **{k: v for k, v in kwargs.items() if isinstance(v, (bool, int, float)) or len(v) != 0})
--- hw3/fastq_filtrator_funcs.py
+++ hw3/fastq_filtrator_funcs.py
@@ -41,21 +41,21 @@ def length_filter(reads, dic_ind, length_bounds=(0, 2 ** 32)):
                 dic_ind[i] += 1
 
 
-def quality_threshold(reads, dic_ind, qs_threshold=0):
+def qs_threshold(reads, dic_ind, quality_threshold=0):
     """
     Filters reads according to their quality score (phred33)
-    qs_threshold - lowest mean quality score for the read
+    quality_threshold - lowest mean quality score for the read
     """
     for i, read in enumerate(reads):
-        qs_sum = 0
+        quality_sum = 0
         for character in read[3]:
-            qs_sum += ord(character) - 33
-        qs_mean = qs_sum / len(read[3])
-        if qs_mean < qs_threshold:
+            quality_sum += ord(character) - 33
+        quality_mean = quality_sum / len(read[3])
+        if quality_mean < quality_threshold:
             dic_ind[i] += 1
 
 
-def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), qs_threshold=0,
+def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0,
          save_filtered=False):
     passed_reads = output_file_prefix + "_passed.fastq"
     failed_reads = output_file_prefix + "_failed.fastq"
@@ -69,8 +69,8 @@ def main(input_fastq, output_file_prefix, gc_bounds=(0, 100), length_bounds=(0, 
             gc_filter(reads, dic_ind, gc_bounds)
         if length_bounds != (0, 2 ** 32):
             length_filter(reads, dic_ind, length_bounds)
-        if qs_threshold != 0:
-            quality_threshold(reads, dic_ind, qs_threshold)
+        if quality_threshold != 0:
+            qs_threshold(reads, dic_ind, quality_threshold)
         for key, value in dic_ind.items():
             if value > 0:
                 if save_filtered:
@@ -96,9 +96,9 @@ if __name__ == "__main__":
     output_path = input("Enter the prefix of the output file: ")
     gc_bounds_in = tuple(float(i) for i in input("Enter the bounds for GC: ").split())
     length_bounds_in = tuple(int(i) for i in input("Enter the bounds for length of the read: ").split())
-    qs_threshold_in = input("Enter the minimal mean quality of the read: ")
+    quality_threshold_in = input("Enter the minimal mean quality of the read: ")
     try:
-        qs_threshold_in = float(qs_threshold_in)
+        quality_threshold_in = float(quality_threshold_in)
     except ValueError:
         pass
     save_filtered_in = bool(int(input("Should reads that failed be saved? Yes - 1, No - 0 \n")))
@@ -107,10 +107,10 @@ if __name__ == "__main__":
     # output_path = "test"
     # gc_bounds_in = tuple(float(i) for i in "40 60".split())
     # length_bounds_in = tuple(float(i) for i in "20 110".split())
-    # qs_threshold_in = float("30")
+    # quality_threshold_in = float("30")
     # save_filtered_in = bool(int("0"))
     kwargs = dict(gc_bounds=gc_bounds_in, length_bounds=length_bounds_in,
-                  qs_threshold=qs_threshold_in, save_filtered=save_filtered_in)
+                  quality_threshold=quality_threshold_in, save_filtered=save_filtered_in)
 
     main(input_path, output_path,
          **{k: v for k, v in kwargs.items() if isinstance(v, (bool, int, float)) or len(v) != 0})
